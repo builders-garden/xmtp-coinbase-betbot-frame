3b7ad9036ccbc68f226bb63ad430217141ea7b71
PaoloRollo <paolo.rollo1997@gmail.com>
2024-08-07 18:08:53 +0200

    feat: update with creation image..

--- app/frames/create-bet-tx/success/route.tsx
+++ app/frames/create-bet-tx/success/route.tsx
@@ -1,7 +1,21 @@
+import { Button } from "frames.js/next";
 import { frames } from "../../frames";
 import { vercelURL } from "@/app/utils";
 
 const handleRequest = frames(async (ctx) => {
+  const buttons = [];
+
+  if (ctx.message?.transactionId) {
+    buttons.push(
+      <Button
+        action="link"
+        target={`https://basescan.org/tx/${ctx.message?.transactionId}`}
+      >
+        View transaction
+      </Button>
+    );
+  }
+
   return {
     image: (
       <div tw="flex flex-col w-[100%] h-[100%]">
@@ -19,6 +33,7 @@ const handleRequest = frames(async (ctx) => {
     headers: {
       "Cache-Control": "public, immutable, no-transform, max-age=0",
     },
+    buttons: [],
   };
 });
 
